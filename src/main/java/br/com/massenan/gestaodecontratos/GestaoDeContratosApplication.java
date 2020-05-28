c8189156a448eed0ab63e28fda7b357e0cb46379
massenandev <massenandev@gmail.com>
2020-05-28 13:06:53 -0300

    artefatos iniciais

---
+++ src/main/java/br/com/massenan/gestaodecontratos/GestaoDeContratosApplication.java
@@ -0,0 +1,13 @@
+package br.com.massenan.gestaodecontratos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeContratosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeContratosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/massenan/gestaodecontratos/GestaoDeContratosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.massenan.gestaodecontratos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeContratosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
