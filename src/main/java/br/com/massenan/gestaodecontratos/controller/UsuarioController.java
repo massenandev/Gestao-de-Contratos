bf5ced23e5186949c2413964e0a871de4368d8f8
massenandev <massenandev@gmail.com>
2020-05-31 19:50:33 -0300

    inclusao de tratamento de erros

--- src/main/java/br/com/massenan/gestaodecontratos/controller/UsuarioController.java
+++ src/main/java/br/com/massenan/gestaodecontratos/controller/UsuarioController.java
@@ -1,32 +1,32 @@
 package br.com.massenan.gestaodecontratos.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.massenan.gestaodecontratos.domain.PerfilEnum;
-import br.com.massenan.gestaodecontratos.domain.Usuario;
 import br.com.massenan.gestaodecontratos.dto.UsuarioDto;
 import br.com.massenan.gestaodecontratos.service.UsuarioService;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	
+	private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
 	@Autowired
 	private UsuarioService service;
-	
+
 	@GetMapping("/listar")
 	public ResponseEntity<?> findAll() {
-		Usuario usr = new Usuario();
-		usr.setAtivo(true);
-		usr.setPendente(true);
-		usr.setPerfil(PerfilEnum.ADMIN);
-		usr.setSenha("123456");
-		usr.setUsername("Leticia");
-		service.create(usr);
-		return ResponseEntity.ok().body(UsuarioDto.parse(service.findAll()));
+		try {
+			return ResponseEntity.ok().body(UsuarioDto.parse(service.findAll()));
+		} catch (Exception ex) {
+			logger.error("[CARREGANDO-TODOS-OS-USUARIOS]", ex.fillInStackTrace());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
