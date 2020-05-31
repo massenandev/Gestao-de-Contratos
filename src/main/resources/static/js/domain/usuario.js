7d02dc216c3f5156e136ccbf37ffdd33be2fd8d8
ronaldo alves rios <ronaldo.alves.rios@gmail.com>
2020-05-31 20:58:23 -0300

    Correcao do usuario.js

--- src/main/resources/static/js/domain/usuario.js
+++ src/main/resources/static/js/domain/usuario.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
 function listaUsuarios(){
 	$.ajax({
 		type:'GET',
-		url: 'localhost:8080/usuarios/listar',
+		url: 'http://localhost:8080/api/usuarios/listar',
 		cache: false,
 		timeOut: 600000,
 		contentType: 'json',
 		statusCode: {
 			200: function(lista){
-				console.log('lista ' + lista);
+				console.log('lista ' + JSON.stringify(lista));
 			},
 			500: function(ex){
 				console.error('Ocorreu um erro.' + ex);
